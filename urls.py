3842cd6c11ac9bdb296202b8194b446f3bb9e1dc
abimgm24-coder <abimgm24@gmail.com>
2026-02-06 21:01:29 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for sms_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from students import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Built-in Login View
+    path('login/', auth_views.LoginView.as_view(template_name='students/login.html'), name='login'),
+    # Custom Logout View
+    path('logout/', views.user_logout, name='logout'),
+    
+    path('', views.home, name='home'),
+    path('add/', views.add_student, name='add_student'),
+    path('edit/<int:id>/', views.edit_student, name='edit_student'),
+    path('delete/<int:id>/', views.delete_student, name='delete_student'),
+]
